ff8ab21fabc77e2840a7aaacb31b25d1b69c0b42
Aritz <arruabarrena.aritz@uni.eus>
2023-03-01 11:43:52 +0100

    Aritz-en-ariketa-mergeatuta

--- project.py
+++ project.py
@@ -1,12 +1,12 @@
 #MENU NAGUSIA, joko bakoitza funtzio baten zortuko da.
-
+import jolasa.tribial
 
 def MENU_NAGUSIA():
     while True:
         print("\nMENUA")
         print("======================")
         print("1 Lehenengo jokoa")#gerson 
-        print("2 Bigarren jokoa")
+        print("2 Tribial(Aritz)")
         print("3 Hirugarren jokoa")
         print("0- Irten")
 
@@ -37,10 +37,10 @@ def jokoa1():
     #JOKO codea
     MENU_NAGUSIA()
 #------------------------------------------------------------------------   
-
+#ARITZ
 def jokoa2():
     #JOKO codea
-    MENU_NAGUSIA()
+    jolasa.tribial.menua()
 #------------------------------------------------------------------------
 
 def jokoa3():
